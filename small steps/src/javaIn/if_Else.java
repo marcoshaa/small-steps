35e87e1ad2d432727d13f2e93b73deee51221e8f
marcoshaa <m.andrade.assis@gmail.com>
2021-07-08 12:28:26 -0300

    frist class if_else

---
+++ small steps/src/javaIn/if_Else.java
@@ -0,0 +1,35 @@
+package javaIn;
+
+import java.util.Scanner;
+
+public class if_Else {
+
+	public static void main(String[] args) {
+		int x;
+		
+		Scanner sc = new Scanner(System.in);
+		
+	System.out.println("inform the time:");
+		x = sc.nextInt();
+		
+		
+		
+		
+	if( x < 12) {
+			System.out.println("Good Morning");
+		
+	}
+	else if(x < 18){
+		System.out.println("Good afternoon");
+	}
+	else if(x < 24 ){
+		
+		System.out.println("Good nigth");
+	}
+	else {
+		System.out.println("inform the time:");
+		x = sc.nextInt();
+	}
+	sc.close();
+	}
+}
