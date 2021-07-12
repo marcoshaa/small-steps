e6a03655dd27061a17b53ee2b4e342a99ea13956
marcoshaa <m.andrade.assis@gmail.com>
2021-07-12 16:15:32 -0300

    Training for the switch_case

--- small steps/src/javaIn/switch_Case.java
+++ small steps/src/javaIn/switch_Case.java
@@ -1,5 +1,45 @@
 package javaIn;
 
+import java.util.Scanner;
+
 public class switch_Case {
 
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int x =sc.nextInt();
+		String day;
+			
+		switch (x) {
+		case 1:
+			day = "Sunday";
+			break;
+		case 2:
+			day = "Monday";
+			break;
+		case 3:
+			day = "Tuesday";
+			break;
+		case 4:
+			day = "Wednesday";
+			break;
+		case 5:
+			day = "Thursday";
+			break;
+		case 6:
+			day = "Friday";
+			break;
+		case 7:
+			day = "Saturday";
+			break;
+			default:
+				day = "value on disability";
+				break;			
+		}
+		
+		System.out.println("Weekday: " + day );
+		
+		sc.close();
+	}
 }
