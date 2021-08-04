cebd61ba1596cc5ee648a6e693a91695b53add68
marcoshaa <m.andrade.assis@gmail.com>
2021-08-03 23:38:44 -0300

    get and set

---
+++ small steps/src/entities/useBank.java
@@ -0,0 +1,25 @@
+package entities;
+
+public class useBank {
+
+	private int nr;
+	private String name;
+	private double value;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getNr() {
+		return nr;
+	}
+	
+	public void setNr(int nr) {
+		this.nr = nr;
+	}
+	
+}
---
+++ small steps/src/exercises/cadBank.java
@@ -0,0 +1,20 @@
+package exercises;
+
+import java.util.Locale;
+import java.util.Scanner;
+import entities.useBank;
+
+public class cadBank {
+
+	
+	public static void mian (String [] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		
+		sc.close();
+	}
+}
