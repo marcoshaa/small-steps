4a7b199fc50a70e6797c50349d7754dc129002aa
marcoshaa <m.andrade.assis@gmail.com>
2021-08-24 23:58:33 -0300

    test

--- small steps/src/entities/supExList.java
+++ small steps/src/entities/supExList.java
@@ -4,11 +4,21 @@ public class supExList {
 
 	private byte id;
 	private String name;
+	private double salary;
 	
-	
-	public supExList(byte id, String name) {
+	public supExList() {
+		
+	}
+	public supExList(byte id, String name, double salary) {
 		this.id = id;
 		this.name = name;
+		this.salary = salary;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
 	}
 	public byte getId() {
 		return id;
@@ -23,7 +33,9 @@ public class supExList {
 		this.name = name;
 	}
 	
-	
+	public void ausalary(Double percSalary) {
+		this.salary = salary + (salary * (percSalary /100));
+	}
 	
 	
 }
--- small steps/src/exercises/exList.java
+++ small steps/src/exercises/exList.java
@@ -14,18 +14,46 @@ public class exList {
 		Scanner sc = new Scanner(System.in);
 		Locale.setDefault(Locale.US);
 		
-		List<String> list = new ArrayList<>();
+		List<supExList> list = new ArrayList<>();
 		
-		System.out.println("How many employees will be registered? ");
+		System.out.print("How many employees will be registered? ");
 		int i = sc.nextInt();
 		
-		for (int o = 1; o <= i; o++ ) {
+		for (int o = 1; o <=i; o++ ) {
 			
 			System.out.println();
 			System.out.println("Emplyooe #"+o+":");
-			
-			
+			System.out.print("Id: ");
+				byte id = sc.nextByte();
+			System.out.print("Name: ");
+			sc.nextLine();
+				String name = sc.nextLine();
+			System.out.print("Salary: ");
+				double salary = sc.nextDouble();
+				
+				supExList exList = new supExList(id,name,salary);
+		list.add(exList);
 		}
+		
+		System.out.print("Enter the employee id that will have salary increase: ");
+		int idsalar = sc.nextInt();
+		Integer po = position(list,idsalar);
+		
+			System.out.println();
+			System.out.print("List of employees: ");
+			
+				System.out.print("");
+						
+		
 	sc.close();
+	}
+	
+	public static Integer hasId(List<supExList> list,int id) {
+	for(int o = 0; o < list.size(); o++) {
+		if(list.get(o).getId()==id) {
+			return o;
+			}
+		}
+	return null;
 	}
 }
