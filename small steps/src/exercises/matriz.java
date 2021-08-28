9623c6a1e72cdf3fb054b268c40973a922d7b444
marcoshaa <m.andrade.assis@gmail.com>
2021-08-27 23:41:18 -0300

    ex. matriz

---
+++ small steps/src/exercises/matriz.java
@@ -0,0 +1,15 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class matriz {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+	sc.close();
+	}
+}
