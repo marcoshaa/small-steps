5fa12cf1bc0b5ebfdd2546f3e8ed418aa4097dac
marcoshaa <m.andrade.assis@gmail.com>
2021-08-29 21:20:31 -0300

    exercicio completo matriz

--- small steps/src/example/matrix.java
+++ small steps/src/example/matrix.java
@@ -11,17 +11,27 @@ public class matrix {
 		int n = sc.nextInt();
 		int[][] mat = new int [n][n];
 		
-		for(int o=0; 0<n; o++) {
+		for(int o=0; o<n; o++) {
 			for(int h=0;h<n;h++) {
 				mat[o][h] = sc.nextInt();			
 			}
 		}
 		
-		System.out.println("main diagonal");
+		System.out.print("main diagonal: ");
 		for(int i =0; i<n; i++) {
-			System.out.print(mat[i][i]+ "");
-			System.out.println();
+			System.out.print(mat[i][i]+ " ");
 		}
+			System.out.println();
+			
+			int con = 0;
+			for(int u = 0; u<n; u++) {
+				for(int o = 0; o<n; o++){
+				if(mat[u][o] < 0) {
+					con++;
+					}
+				}
+		}
+			System.out.print("Negative numbers: "+con);
 		sc.close();	
 	}
 
