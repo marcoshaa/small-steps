d86ae9d398538457a685fd695f907d421a723d3d
marcoshaa <m.andrade.assis@gmail.com>
2021-09-17 23:39:14 -0300

    calc

---
+++ small steps/src/test/tT.java
@@ -0,0 +1,22 @@
+package test;
+
+import java.util.Scanner;
+
+public class tT {
+
+	public static void mian(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Entre com um numero: ");
+		int n1 = sc.nextInt();
+		
+		for(int i=0;i<11;i++ ) {
+			
+			int n1;
+			
+		}
+		
+	sc.close();
+	}
+}
