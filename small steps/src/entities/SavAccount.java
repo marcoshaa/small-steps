0cde8aac8b9e8729f74aa41b8aa884aba433dffa
marcoshaa <m.andrade.assis@gmail.com>
2021-09-21 22:39:21 -0300

    UPCASTING

---
+++ small steps/src/entities/SavAccount.java
@@ -0,0 +1,27 @@
+package entities;
+
+public class SavAccount extends account{
+
+	private Double interestRate;
+	
+	public SavAccount() {
+		super();
+	}
+
+	public SavAccount(Integer number, String holde, Double balance, Double interestRate) {
+		super(number, holde, balance);
+		this.interestRate = interestRate;
+	}
+
+	public Double getInterestRate() {
+		return interestRate;
+	}
+
+	public void setInterestRate(Double interestRate) {
+		this.interestRate = interestRate;
+	}
+	
+	public void updateBalance() {
+		balance += balance * interestRate;
+	}
+}
--- small steps/src/heritage/program.java
+++ small steps/src/heritage/program.java
@@ -1,10 +1,19 @@
 package heritage;
 
+import entities.BusinessAccount;
+import entities.SavAccount;
+import entities.account;
+
 public class program {
 	
 	public static void main (String[]args) {
 		
+		account acc = new account(1000, "Alex", 0.0);
+		BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
 		
+		account acc1 = bacc;
+		account acc2 = new BusinessAccount(1003, "Fernanda", 0.0, 300.0);
+		account acc3 = new SavAccount(1004, "Anna", 500.00, 0.01); 
 	}
 
 }
