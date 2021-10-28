c4412b0a6864efdb05e0561b3dd7888c08535b92
marcoshaa <m.andrade.assis@gmail.com>
2021-10-27 23:58:25 -0300

    new class

---
+++ small steps/ex_fix/entitis/pes_Fisica.java
@@ -0,0 +1,40 @@
+package entitis;
+
+public class pes_Fisica {
+	
+	private String name;
+	private double rend;
+	private double med;
+
+	public pes_Fisica() {
+		
+
+	}
+	public pes_Fisica(String name, double rend, double med) {
+		this.name = name;
+		this.rend = rend;
+		this.med = med;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getRend() {
+		return rend;
+	}
+	public void setRend(double rend) {
+		this.rend = rend;
+	}
+	public double getMed() {
+		return med;
+	}
+	public void setMed(double med) {
+		this.med = med;
+	}
+	
+	public void IrPF(double ) {
+		
+	}
+}
